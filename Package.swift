7c233204c4c581765ae8a32074dd1e9dd8c33d1f
Sam Rayner <sam@samrayner.com>
2022-09-14 14:00:59 +0100

    Point at Embassy fork for Xcode 14 fix

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "Ambassador", targets: ["Ambassador"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/envoy/Embassy.git", from: "4.0.5")
+        .package(url: "https://github.com/JRR-OSU/Embassy-OSU.git", branch: "fix-crash-memory-rebound")
     ],
     targets: [
         .target(name: "Ambassador", dependencies: ["Embassy"], path: "Ambassador"),
